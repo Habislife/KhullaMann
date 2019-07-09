94ed0286c44ec4e2bc9912edf441629541923103
gurungena13 <gurungena13@gmail.com>
2019-07-09 21:45:26 +0545

    Homepage

--- index.php
+++ index.php
@@ -2,28 +2,58 @@
 <html>
 <head>
 	<title>Khulla mann homepage</title>
-	<link rel="stylesheet" type="text/css" href="css/homepage.css">
+	<link rel="stylesheet" type="text/css" href="css/index.css">
 </head>
 <body>
-	<div class="container">
-      <div class="logo"></div> 
-        <div class="header">
-            <ul>
-                <li><a href="html/RegisterForm.html">Become a donor</a></li>
-                <li><a href="html/LoginForm.html">Login</a></li>
-                <li><a href="html/DoneeReg.html">Sign up</a></li>
-            </ul>
-        </div>
-         <div class="navbar">   
-            <nav>
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About us</a></li> 
-                    <li><a href="#">Activity</a></li>
-                </ul>
-            </nav>
-          </div>
-    </div>
+  <div class=header>
+    <div class="logo"></div>
+    <div class="head">
+        <input type="text" name="text" placeholder="Search.." size="20px">
+        <br>
+        <a href="html/RegisterForm.html">Become a Donor</a>
+        <a href="html/DoneeReg.html">Sign Up</a>
+        <a href="html/LoginForm.html">Login</a>
+      </div>
+  </div>
 
+  <nav id="navbar">
+    <ul id="nav">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About Us</a></li>
+      <li><a href="#">Discussion</a></li>    
+      <li><a href="#">Contact Us</a></li>
+    </ul>
+  </nav>
+
+  <div class="slideshow-container">
+      <div class="slides fade">
+         <img src="img1.jpeg">
+      </div>
+
+      <div class="slides fade">
+        <img src="img2.jpg">
+      </div>
+
+      <div class="slides fade">
+       <img src="img3.jpeg">
+      </div>
+  </div>
+  <br>
+
+  <div class="content">
+    <div class="article">
+      <h1>ARTICLE 1</h1><br><span>vbebjknrknerv</span>
+    </div>
+    <div class="article">
+      <h1>ARTICLE 2</h1><br><span>vvrbevgvbtbtrbtrb</span>
+    </div>
+    <div class="article">
+      <h1>ARTICLE 3</h1><br><span>bfdvehverhbhrttrr</span>
+    </div>
+  </div>
+
+  <div class="footer"></div>
+
+  <script type="text/javascript" src="js/index.js"></script>
 </body>
 </html>
