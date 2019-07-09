c993c8bcba16591b6fa77b7e4955f5085c6c1e02
Bishal Devkota <devkotahancy@gmail.com>
2019-07-09 19:48:22 +0545

    bv

--- js/RegisterForm.js
+++ js/RegisterForm.js
@@ -1,50 +1,10 @@
-    var username = document.getElementById('username').value;
-    var email = document.getElementById('email').value;
-    var address = document.getElementById('address').value;
-    var contact = document.getElementById('contact').value;
-    var password = document.getElementById('password').value;
-    var repassword = document.getElementById('repassword').value;
-   var errorMessage = document.getElementById('errorMessage');
-     function validateform()
-     {
-    if( username=="" || username==null){
-        // errorMessage.textContent = "Username can't be empty";
-        alert("Name can't be blank");
-        return false
-    }  
-
-    if(email=="" || email==null){
-        errorMessage.textContent = "Email can't be empty";
-        return false
-    }
-  
-    if(address==""|| email==null){
-        errorMessage.textContent = "Address can't be empty";
-    }
-    
-    if(contact=="" || email==null ){
-        errorMessage.textContent = "Contact can't be empty";
-    }
-
-    if(password=="" || email==null){
-        errorMessage.textContent = "Password can't be empty";
-
-    }
-
-    if(repassword=="" || email==null){
-        errorMessage.textContent = "Password re-type can't be empty";
-        return false
-    
-    }
-     
-    if(!document.getElementById('agree').checked){
-        alert("You have agree our terms and conditions in order to proceed.");
-        return false
-    }
+var check = function() {
+  if (document.getElementById('password').value ==
+    document.getElementById('repassword').value) {
+    document.getElementById('message').style.color = 'green';
+    document.getElementById('message').innerHTML = 'matching';
+  } else {
+    document.getElementById('message').style.color = 'red';
+    document.getElementById('message').innerHTML = 'not matching';
+  }
 }
-
-    function checkPassword(){
-        if(repassword!=password){
-            document.getElementById('repassword').style.color = 'red';
-        }
-    }
