32bda7d2e5de14a27708f8fac0a8fe9ec6c83a2a
pabita_pun <pabita.pun23@gmail.com>
2019-07-14 00:02:25 +0545

    js change

--- js/LoginForm.js
+++ js/LoginForm.js
@@ -1,3 +1,4 @@
+/*
 var email = document.getElementById('email').value;
 var password = document.getElementById('password').value;
 var errorMessage = document.getElementById('errorMessage');
@@ -17,4 +18,22 @@ function checkform() {
 
     return true;
 }
+*/
 
+var checkform = function() {
+    if(document.getElementById('email').value == "") {
+        document.getElementById('message').style.color = 'red';
+        document.getElementById('message').innerHTML = 'Email cannot be empty';
+    } else {
+        document.getElementById('message').style.color = 'green';
+        document.getElementById('message').innerHTML = 'Email is not empty';
+    }
+    
+    if(document.getElementById('password').value == "") {
+        document.getElementById('message1').style.color = 'red';
+        document.getElementById('message1').innerHTML = 'Password cannot be empty';
+    } else {
+        document.getElementById('message1').style.color = 'green';
+        document.getElementById('message1').innerHTML = 'Password is not empty';
+    }
+}
--- js/RegisterForm.js
+++ js/RegisterForm.js
@@ -1,6 +1,6 @@
 var check = function() {
-  if (document.getElementById('password').value ==
-    document.getElementById('repassword').value) {
+  if (document.getElementById('password').value == 
+      document.getElementById('repassword').value) {
     document.getElementById('message').style.color = 'green';
     document.getElementById('message').innerHTML = 'matching';
   } else {
@@ -11,27 +11,27 @@ var check = function() {
 
 var fieldCheck = function() {
     if(document.getElementById('username').value == "") {
-        document.getElementById('message').style.color = 'red';
-        document.getElementById('message').innerHTML = 'Username cannot be empty';
+        document.getElementById('message1').style.color = 'red';
+        document.getElementById('message1').innerHTML = 'Username cannot be empty';
     } else {
-        document.getElementById('message').style.color = 'green';
-        document.getElementById('message').innerHTML = 'Username is not empty';
+        document.getElementById('message1').style.color = 'green';
+        document.getElementById('message1').innerHTML = 'Username is not empty';
     }
     
     if(document.getElementById('email').value == "") {
-        document.getElementById('message').style.color = 'red';
-        document.getElementById('message').innerHTML = 'Email cannot be empty';
+        document.getElementById('message2').style.color = 'red';
+        document.getElementById('message2').innerHTML = 'Email cannot be empty';
     } else {
-        document.getElementById('message').style.color = 'green';
-        document.getElementById('message').innerHTML = 'Email is not empty';
+        document.getElementById('message2').style.color = 'green';
+        document.getElementById('message2').innerHTML = 'Email is not empty';
     }
     
     if(document.getElementById('password').value == "") {
-        document.getElementById('message').style.color = 'red';
-        document.getElementById('message').innerHTML = 'Password cannot be empty';
+        document.getElementById('message3').style.color = 'red';
+        document.getElementById('message3').innerHTML = 'Password cannot be empty';
     } else {
-        document.getElementById('message').style.color = 'green';
-        document.getElementById('message').innerHTML = 'Password is not empty';
+        document.getElementById('message3').style.color = 'green';
+        document.getElementById('message3').innerHTML = 'Password is not empty';
     }
     if(document.getElementById('repassword').value == "") {
         document.getElementById('message').style.color = 'red';
@@ -41,18 +41,79 @@ var fieldCheck = function() {
         document.getElementById('message').innerHTML = 'Repassword is not empty';
     }
     if(document.getElementById('address').value == "") {
-        document.getElementById('message').style.color = 'red';
-        document.getElementById('message').innerHTML = 'Address cannot be empty';
+        document.getElementById('message4').style.color = 'red';
+        document.getElementById('message4').innerHTML = 'Address cannot be empty';
     } else {
-        document.getElementById('message').style.color = 'green';
-        document.getElementById('message').innerHTML = 'Address is not empty';
+        document.getElementById('message4').style.color = 'green';
+        document.getElementById('message4').innerHTML = 'Address is not empty';
     }
     if(document.getElementById('contact').value == "") {
-        document.getElementById('message').style.color = 'red';
-        document.getElementById('message').innerHTML = 'Contact cannot be empty';
+        document.getElementById('message5').style.color = 'red';
+        document.getElementById('message5').innerHTML = 'Contact cannot be empty';
     } else {
-        document.getElementById('message').style.color = 'green';
-        document.getElementById('message').innerHTML = 'Contact is not empty';
+        document.getElementById('message5').style.color = 'green';
+        document.getElementById('message5').innerHTML = 'Contact is not empty';
     }
     
+/*var pass = document.getElementById('password')
+pass.addEventListener('keyup', function() {
+    checkPassword(pass.value)
+}) 
+
+function checkPassword(password) {
+    var strenthBar = document.getElementById('strength')
+    var strength = 0;
+    if(password.match(/[a-zA-Z0-9][a-zA-Z0-9]+/)) {
+        strength += 1
+    }
+    if(password.match(/[~<>?]+/)) {
+        strength += 1
+    }
+    if(password.match(/[!@#$%^&*()]+/)) {
+        strength += 1
+    }
+    if(password.length > 5) {
+        strength += 1
+    }
+    switch(strength) {
+        case 0:
+                strengthBar.value = 20;
+                break
+        case 1: 
+                strengthBar.value = 40;
+                break
+        case 2: 
+                strengthBar.value = 60;
+                break
+        case 3: 
+                strengthBar.value = 80;
+                break
+        case 4: 
+                strengthBar.value = 100;
+                break
+    }
+}*/
+    
 }
+
+
+
+
+
+/*var checkemail = function() {
+    if((document.getElementById('email').value).indexOf('@'<=0)) {
+        document.getElementById('message2').style.color = 'red';
+        document.getElementById('message2').innerHTML = 'email is invalid';
+    } else {
+        document.getElementById('message2').style.color = 'green';
+        document.getElementById('message2').innerHTML = 'email is valid';
+    }
+    
+    if((document.getElementById('email').value).charAt((document.getElementById('email').value).length-4)!='.') && ((document.getElementById('email').value).charAt((document.getElementById('email').value)length-3)!='.') {
+        document.getElementById('message2').style.color = 'red';
+        document.getElementById('message2').innerHTML = 'invalid . position of 4';
+    } else {
+        document.getElementById('message2').style.color = 'green';
+        document.getElementById('message2').innerHTML = 'valid . position';
+    }
+}*/
