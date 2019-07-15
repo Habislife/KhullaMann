6384a5f18698b152305dc42a629f693b472efa4f
Bishal Devkota <devkotahancy@gmail.com>
2019-07-15 19:23:47 +0545

    fhbfbf

---
+++ khullamann.sql
@@ -0,0 +1,129 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 15, 2019 at 03:38 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 5.6.40
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `khullamann`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `donation_post`
+--
+
+CREATE TABLE `donation_post` (
+  `post_id` int(11) NOT NULL,
+  `file_path` text NOT NULL,
+  `bio` text NOT NULL,
+  `description` text NOT NULL,
+  `donee_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doneeprofile`
+--
+
+CREATE TABLE `doneeprofile` (
+  `donee_id` int(11) NOT NULL,
+  `username` varchar(10) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `password` text NOT NULL,
+  `address` varchar(5) NOT NULL,
+  `contact_no` bigint(20) NOT NULL,
+  `account_no` bigint(20) NOT NULL,
+  `document` longblob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `donorprofile`
+--
+
+CREATE TABLE `donorprofile` (
+  `donor_id` int(11) NOT NULL,
+  `username` varchar(10) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `password` text NOT NULL,
+  `address` varchar(5) NOT NULL,
+  `contact_no` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `donation_post`
+--
+ALTER TABLE `donation_post`
+  ADD PRIMARY KEY (`post_id`),
+  ADD KEY `donee_id` (`donee_id`);
+
+--
+-- Indexes for table `doneeprofile`
+--
+ALTER TABLE `doneeprofile`
+  ADD PRIMARY KEY (`donee_id`);
+
+--
+-- Indexes for table `donorprofile`
+--
+ALTER TABLE `donorprofile`
+  ADD PRIMARY KEY (`donor_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `donation_post`
+--
+ALTER TABLE `donation_post`
+  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `doneeprofile`
+--
+ALTER TABLE `doneeprofile`
+  MODIFY `donee_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `donorprofile`
+--
+ALTER TABLE `donorprofile`
+  MODIFY `donor_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `donation_post`
+--
+ALTER TABLE `donation_post`
+  ADD CONSTRAINT `donation_post_ibfk_1` FOREIGN KEY (`donee_id`) REFERENCES `doneeprofile` (`donee_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
