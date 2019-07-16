35a822eee0a8371f3059058120a864e568758d9d
Bishal Devkota <devkotahancy@gmail.com>
2019-07-16 20:58:23 +0545

    jhnj

---
+++ php/Loginserver.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+if(isset($_POST['submit']))
+{
+ $username = $_POST['username'];
+ $password = $_POST['password'];
+ $conn = mysqli_connect("localhost","root","","firphire");
+  $username = mysqli_real_escape_string($connection,$username);
+    $password = mysqli_real_escape_string($connection,$password);
+    $cpassword=md5($password);
+    
+
+    $query = "SELECT * FROM doneeprofile WHERE username  = '{$username}' AND password ='{$cpassword}' ";
+
+    $select_user_query = mysqli_query($connection,$query);
+     if(!$select_user_query)
+    {
+        die("QUERY FAILED".mysqli_error($connection));
+    }
+
+    $row = mysqli_fetch_array($select_user_query);
+    $_SESSION['userid']=$row['userid'];
+    if( $username == $row['username']  )
+    {
+    	  if($cpassword == $row['password'])
+    	  {
+    	  	header("Location: ../html/DoneeHomepage.html");
+    	  }
+
+    }
+       $query = "SELECT * FROM donorprofile WHERE username  = '{$username}' AND password ='{$cpassword}' ";
+
+    $select_user_query = mysqli_query($connection,$query);
+     if(!$select_user_query)
+    {
+        die("QUERY FAILED".mysqli_error($connection));
+    }
+
+    $row = mysqli_fetch_array($select_user_query);
+    $_SESSION['userid']=$row['userid'];
+    if( $username == $row['username']  )
+    {
+    	  if($cpassword == $row['password'])
+    	  {
+    	  	header("Location: ../html/DonorHomepage.html");
+    	  }
+
+    }
+
+
+}
+?>
