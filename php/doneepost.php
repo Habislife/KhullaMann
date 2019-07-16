38a2564d765abb29a140801c6372ab0c8357934b
pabita_pun <pabita.pun23@gmail.com>
2019-07-16 20:56:11 +0545

    php

--- php/doneepost.php
+++ php/doneepost.php
@@ -4,11 +4,34 @@ $username = "root";
 $password = "";
 $dbname = "khullamann";
 
-//connection created
+//connection creation 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-//connection checking
-if ($conn->connection_error) {
-    die("Connection failed: " . $conn->connection_error);
-    Creation();
+//check connection
+if($conn->connect_error)
+{
+    die("connection failed: ".$conn->connect_error);
 }
+/*else
+{
+    echo "connection successful";
+}*/
+
+//values that will be inserted
+
+//$file_path = $_POST['file_path'];
+$bio = $_POST['bio'];
+$description = $_POST['description']; 
+
+
+//insert datas in database
+$data = "INSERT INTO donation_post(bio, description) VALUES ('$bio', '$description')";
+
+if ($conn->query($data) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $data . "<br>" . $conn->connect_error;
+}
+
+$conn->close();
+?>
