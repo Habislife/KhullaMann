0c6bb51e1d10747fa212c584ff6ad0bcaa0ad6d8
Bishal Devkota <devkotahancy@gmail.com>
2019-07-21 21:54:40 +0545

    Signed-off-by: Bishal Devkota <devkotahancy@gmail.com>

--- sqlFile/khullamann.sql
+++ sqlFile/khullamann.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 21, 2019 at 01:35 PM
+-- Generation Time: Jul 21, 2019 at 06:06 PM
 -- Server version: 10.3.15-MariaDB
 -- PHP Version: 7.3.6
 
