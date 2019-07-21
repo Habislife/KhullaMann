4e77fad8c077759089c15867773790d36e6eb047
Bishal Devkota <devkotahancy@gmail.com>
2019-07-21 10:11:30 +0545

    dddddddddddddd

--- src/db/doneeRegister.php
+++ src/db/doneeRegister.php
@@ -1,5 +1,9 @@
 <?php
 include 'connection.php';
+$target_dir =$_SERVER['DOCUMENT_ROOT']."/khullamann/upload/images/post/";
+$target_file = $target_dir . basename($_FILES["image"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 if(isset($_POST['submit']))
 {
     $email = $_POST['email'];
@@ -9,12 +13,47 @@ if(isset($_POST['submit']))
     $contact = $_POST['contact'];
     $account=$_POST['account'];
     $cpassword=md5($password);
+    $check = getimagesize($_FILES["image"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+    $bio = $_POST['bio'];
+    $description = $_POST['description'];
+    $today = date("Y/m/d");
+    $time=date("h:m:s");
+    if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["image"]["size"] >2000000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["image"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+} 
      $sql = "INSERT INTO doneeprofile (username,email,password,address,contact_no,account_no) VALUES ('$username','$email','$cpassword','$address','$contact','$account')";
  $result = mysqli_query($conn,$sql);
 header('Location:../../LoginForm.php');
 $conn->close();
-   
-    
-
 }
 ?>
