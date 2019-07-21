bc12d75cb0bd2a585f5e51e4dba3a69dcf5593ca
Bishal Devkota <devkotahancy@gmail.com>
2019-07-21 08:54:46 +0545

    sssss

--- src/db/databaseCreation.php
+++ src/db/databaseCreation.php
@@ -1,12 +1,5 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-
-// Check connection
+include 'connection.php';
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
