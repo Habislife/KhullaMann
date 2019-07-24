39c6817fd8959ddebe8c096285651d91a6f31134
Bishal Devkota <devkotahancy@gmail.com>
2019-07-24 16:04:20 +0545

    123456

--- sqlFile/khullamann.sql
+++ sqlFile/khullamann.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 21, 2019 at 06:06 PM
+-- Generation Time: Jul 24, 2019 at 12:18 PM
 -- Server version: 10.3.15-MariaDB
 -- PHP Version: 7.3.6
 
@@ -73,6 +73,22 @@ CREATE TABLE `donorprofile` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Dumping data for table `donorprofile`
+--
+
+INSERT INTO `donorprofile` (`donor_id`, `profile_pic`, `username`, `email`, `password`, `address`, `contact_no`) VALUES
+(1, '', 'cbriggs0', 'croll0@elpais.com', '7VcVgx3O8tX', '98208 Schiller Drive', 755),
+(2, '', 'rmathes1', 'seddow1@cpanel.net', 'LEnNJxbJ46O', '29 Lien Lane', 320),
+(3, '', 'bcoode2', 'dgurko2@devhub.com', 'B8V4r41V', '4 Cordelia Pass', 107),
+(4, '', 'jstutard3', 'ljuares3@fema.gov', 'ECyvv61EJmD0', '8797 Tomscot Way', 618),
+(5, '', 'bhoggan4', 'npavek4@cornell.edu', 'wLqyMNy2bcEq', '1687 Haas Alley', 419),
+(6, '', 'epettiward5', 'poldam5@google.ca', '6aHjyJb', '11 Hoard Hill', 569),
+(7, '', 'mfinder6', 'olumber6@tinypic.com', 'dWyqun7IRcsb', '331 Stephen Junction', 124),
+(8, '', 'mgross7', 'rpires7@foxnews.com', 'sc7bIjPyg', '38 Meadow Ridge Junction', 454),
+(9, '', 'cdye8', 'phovey8@is.gd', 'MOi3QBcMAWa', '9397 Monument Road', 768),
+(10, '', 'cokuddyhy9', 'vfowells9@mtv.com', 'iTHRzo3Vrr14', '19278 Mayer Point', 330);
+
+--
 -- Indexes for dumped tables
 --
 
@@ -115,7 +131,7 @@ ALTER TABLE `doneeprofile`
 -- AUTO_INCREMENT for table `donorprofile`
 --
 ALTER TABLE `donorprofile`
-  MODIFY `donor_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `donor_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Constraints for dumped tables
