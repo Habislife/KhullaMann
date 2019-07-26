b040e7ec9cca7bb2fc80a8d73c8665455786952f
Bishal Devkota <devkotahancy@gmail.com>
2019-07-26 13:49:19 +0545

    vvv

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Khulla mann homepage</title>
+	<link rel="stylesheet" type="text/css" href="src/css/index.css">
+</head>
+<body>
+  <?php include'NavBar.php' ?>
+
+  <div style="margin-top: 50px;">
+      <img  src="src/images/photo.png" style="width: 100%;">
+    </div>
+    <br>
+
+    <div class="content">
+
+      <div class="card">
+       <a href="#"><img src="src/images/anes-sabitovic-AyflGfRL9OA-unsplash.jpg" alt="Avatar" style="width:100%"></a>
+        <h1>Refugee Campaign</h1>
+        <p class="bio">Help the refugees.</p>
+         <p>Some text about the jeans. Super slim and comfy lorem ipsum lorem jeansum. Lorem jeamsun denim lorem  Super slim and cfy um v.uper slim and cfy um vuper slim and cfy um upanvvdsddvdvsddvds</p>
+        <a href="#"><button>Click to View</button></a>
+    </div>
+      
+
+      <div class="card">
+         <a href="#"><img src="src/images/anes-sabitovic-AyflGfRL9OA-unsplash.jpg" alt="Avatar" style="width:100%"></a>
+        <h1>Refugee Campaign</h1>
+        <p class="bio">Help the refugees.</p>
+         <p>Some text about the jeans. Super slim and comfy lorem ipsum lorem jeansum. Lorem jeamsun denim lorem  Super slim and cfy um v.uper slim and cfy um vuper slim and cfy um upanvv</p>
+        <a href="#"><button>Click to View</button></a>
+    </div>
+
+      <div class="card">
+        <a href="#"><img src="src/images/anes-sabitovic-AyflGfRL9OA-unsplash.jpg" alt="Avatar" style="width:100%"></a>
+        <h1>Refugee Campaign</h1>
+        <p class="bio">Help the refugees.</p>
+         <p>Some text about the jeans. Super slim and comfy lorem ipsum lorem jeansum. Lorem jeamsun denim lorem  Super slim and cfy um v.uper slim and cfy um vuper slim and cfy um upanvv</p>
+        <a href="#"><button>Click to View</button></a>
+    </div>
+    
+    </div>
+
+  <?php include'Footer.php' ?>
+
+    <script type="text/javascript" src="src/js/index.js"></script>
+</body>
+</html>
