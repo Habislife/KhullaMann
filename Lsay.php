4d40a8c31b90f34ae4e725dc2e6b9429b8ea5a94
pabita_pun <pabita.pun23@gmail.com>
2019-07-26 22:51:46 +0545

    again change

--- Lsay.php
+++ Lsay.php
@@ -1,12 +1,12 @@
 <?php 
-include '../../connection.php';
+include 'src/db/connection.php';
 include 'mail.php';
 if (isset($_POST['submit'])) {
-	$feedback_email = $_POST['feedback_email'];
-	$subject = $_POST['subject'];
-    $feedback_content = $_POST['feedback_content'];
+	$email = $_POST['email'];
+	$sub = $_POST['sub'];
+    $content = $_POST['content'];
     $email = mysqli_real_escape_string($conn,$feedback_email,$subject,$feedback_content);
-    $query = "INSERT * INTO feedback";
+    $query = "INSERT INTO feedback(feedback_email,subject,feedback_content) VALUES($email,$sub,$content) ";
     $select_user_query = mysqli_query($conn,$query);
     if(!$select_user_query)
     {
@@ -31,11 +31,11 @@ if (isset($_POST['submit'])) {
 </head>
 <body>
 	<form method="POST">
-		<input type="email" name="email" placeholder="My email" id="feedback_email">
+		<input type="email" name="email" placeholder="My email" id="email">
 		<br><br>
-		<input type="text" name="subject" placeholder="Subject" id="subject">
+		<input type="text" name="subject" placeholder="Subject" id="sub">
 		<br><br>
-		<textarea rows="5" name="givefeedback" placeholder="Give feedback..." id="feedback_content"></textarea>
+		<textarea rows="5" name="givefeedback" placeholder="Give feedback..." id="content"></textarea>
 		<br><br>
 		<button type="submit" name="submit" value="submit">Send</button>
 	</form>
