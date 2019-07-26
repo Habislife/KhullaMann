20c41724eb8e1e13e03fa5c6781af7143c6d245b
pabita_pun <pabita.pun23@gmail.com>
2019-07-26 21:57:07 +0545

    lsay change

--- Lsay.php
+++ Lsay.php
@@ -1,10 +1,11 @@
 <?php 
-include '../connection.php';
+include '../../connection.php';
 include 'mail.php';
 if (isset($_POST['submit'])) {
-	$feedback_email = $_POST['email'];
-    $
-    $email = mysqli_real_escape_string($conn,$email);
+	$feedback_email = $_POST['feedback_email'];
+	$subject = $_POST['subject'];
+    $feedback_content = $_POST['feedback_content'];
+    $email = mysqli_real_escape_string($conn,$feedback_email,$subject,$feedback_content);
     $query = "INSERT * INTO feedback";
     $select_user_query = mysqli_query($conn,$query);
     if(!$select_user_query)
@@ -14,16 +15,11 @@ if (isset($_POST['submit'])) {
 
     $row = mysqli_fetch_array($select_user_query);
 
-
-
-
-
-
 	$header=$_POST['email'];
 	$subject=$_POST['subject'];
 	$message=$_POST['givefeedback'];
-	$email="khullamann5660@gmail.com";
-$mail= new Mail($email,$subject,$message,$header);	
+	$to_email="khullamann5660@gmail.com";
+    $mail= new Mail($to_email,$subject,$message,$header);	
 }
 
 ?>
@@ -35,11 +31,11 @@ $mail= new Mail($email,$subject,$message,$header);
 </head>
 <body>
 	<form method="POST">
-		<input type="email" name="email" placeholder="My email">
+		<input type="email" name="email" placeholder="My email" id="feedback_email">
 		<br><br>
-		<input type="text" name="subject" placeholder="Subject">
+		<input type="text" name="subject" placeholder="Subject" id="subject">
 		<br><br>
-		<textarea rows="5" name="givefeedback" placeholder="Give feedback..."></textarea>
+		<textarea rows="5" name="givefeedback" placeholder="Give feedback..." id="feedback_content"></textarea>
 		<br><br>
 		<button type="submit" name="submit" value="submit">Send</button>
 	</form>
