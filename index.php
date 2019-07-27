4ad47d30e321c626fcdf805e999bdb774a1100f0
gurungena13 <gurungena13@gmail.com>
2019-07-27 13:09:19 +0545

    post page

--- index.php
+++ index.php
@@ -19,27 +19,27 @@
         <h1>Refugee Campaign</h1>
         <p class="bio">Help the refugees.</p>
          <p>Some text about the jeans. Super slim and comfy lorem ipsum lorem jeansum. Lorem jeamsun denim lorem  Super slim and cfy um v.uper slim and cfy um vuper slim and cfy um upanv</p>
-        <a href="#"><button id="viewbtn">View</button></a>
-        <a href="#"><button id="donatebtn">Donate</button></a>
+        <a href="post.php"><button id="viewbtn">View</button></a>
+        <a href="LoginForm.php"><button id="donatebtn">Donate</button></a>
     </div>
       
 
       <div class="card">
-         <a href="#"><img src="src/images/anes-sabitovic-AyflGfRL9OA-unsplash.jpg" alt="Avatar" style="width:100%"></a>
+       <a href="#"><img src="src/images/anes-sabitovic-AyflGfRL9OA-unsplash.jpg" alt="Avatar" style="width:100%"></a>
         <h1>Refugee Campaign</h1>
         <p class="bio">Help the refugees.</p>
-         <p>Some text about the jeans. Super slim and comfy lorem ipsum lorem jeansum. Lorem jeamsun denim lorem  Super slim and cfy um v.uper slim and cfy um vuper slim and cfy um upanvv</p>
-        <a href="#"><button id="viewbtn">View</button></a>
-        <a href="#"><button id="donatebtn">Donate</button></a>
+         <p>Some text about the jeans. Super slim and comfy lorem ipsum lorem jeansum. Lorem jeamsun denim lorem  Super slim and cfy um v.uper slim and cfy um vuper slim and cfy um upanv</p>
+        <a href="post.php"><button id="viewbtn">View</button></a>
+        <a href="LoginForm.php"><button id="donatebtn">Donate</button></a>
     </div>
-
+    
       <div class="card">
-        <a href="#"><img src="src/images/anes-sabitovic-AyflGfRL9OA-unsplash.jpg" alt="Avatar" style="width:100%"></a>
+       <a href="#"><img src="src/images/anes-sabitovic-AyflGfRL9OA-unsplash.jpg" alt="Avatar" style="width:100%"></a>
         <h1>Refugee Campaign</h1>
         <p class="bio">Help the refugees.</p>
-         <p>Some text about the jeans. Super slim and comfy lorem ipsum lorem jeansum. Lorem jeamsun denim lorem  Super slim and cfy um v.uper slim and cfy um vuper slim and cfy um upanvv</p>
-       <a href="#"><button id="viewbtn">View</button></a>
-        <a href="#"><button id="donatebtn">Donate</button></a>
+         <p>Some text about the jeans. Super slim and comfy lorem ipsum lorem jeansum. Lorem jeamsun denim lorem  Super slim and cfy um v.uper slim and cfy um vuper slim and cfy um upanv</p>
+        <a href="post.php"><button id="viewbtn">View</button></a>
+        <a href="LoginForm.php"><button id="donatebtn">Donate</button></a>
     </div>
 
 
---
+++ post.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+body {
+  font-family: Arial;
+  color: white;
+}
+
+.split {
+  height: 90%;
+  width: 50%;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  overflow-x: hidden;
+  padding-top: 20px;
+}
+
+.left {
+	left: 0;
+  background-image: url(src/images/postpic.jpg);
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  
+}
+
+.right {
+  right: 0;
+  
+}
+
+.centered {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  color: black;
+}
+
+
+</style>
+</head>
+<body>
+
+<?php include 'Nav.html' ?> 
+
+<div class="split right">
+  <div class="centered" >
+    <h1>Music Concert</h1>
+    <br>
+    <p align="justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse </p>
+    <br>
+    <label>$100</label>
+    <button>Donate</button>
+    <div style="margin: 24px 0;">
+    <a href="#"><i class="fa fa-dribbble"></i></a> 
+    <a href="#"><i class="fa fa-twitter"></i></a>  
+    <a href="#"><i class="fa fa-linkedin"></i></a>  
+    <a href="#"><i class="fa fa-facebook"></i></a> 
+  </div>
+  </div>
+</div>
+
+<div class="split left">
+  <div class="centered">
+    
+  </div>
+</div>
+     
+</body>
+</html> 
