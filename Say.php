e6e77f91dc148b4834b2ec5101273a7e57a08090
pabita_pun <pabita.pun23@gmail.com>
2019-07-28 19:34:15 +0545

    say.php completed

--- Say.php
+++ Say.php
@@ -1,34 +1,18 @@
 <?php
 include 'src/db/connection.php';
-include 'mail.php';
+include 'src/db/mail.php';
+
 if (isset($_POST['submit'])) {
-  $header= $_POST['email'];
-  $email = mysqli_real_escape_string($conn,$header);
-  $sql = "SELECT * FROM donorprofile WHERE email = '{$email}'";
-    $select_user_query = mysqli_query($conn,$sql);
-    if(!$select_user_query)
-    {
-        die("QUERY FAILED".mysqli_error($conn));
-    }
-
-    $row = mysqli_fetch_array($select_user_query);
-    if( $email == $row['email'] )
-    {
-     $subject=$_POST['subject'];
-     $message=$_POST['givefeedback']; 
-      
-      $mail= new Mail($to_email,$subject,$message,$header); 
-      echo "Hey "; 
-    }
-    else{
-      echo "Email unavailable";
-    }
-
   $to_email="khullamann5660@gmail.com";
+  session_start();
+  $header = $SESSION['email'];
+  $subject = $_POST['subject'];
+  $message = $_POST['givefeedback']; 
+  $mail = new Mail($to_email,$subject,$message);
+  $query = "INSERT INTO feedback(feedback_email,subject,feedback_content) VALUES('$header','$subject','$message')";
     
-    $query = "INSERT INTO feedback(feedback_email,subject,feedback_content) VALUES('$header','$subject','$message')";
-    
-    $result = mysqli_query($conn,$query);
+  $result = mysqli_query($conn,$query);
+
 } 
 ?>
 
