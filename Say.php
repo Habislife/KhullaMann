b011cd8b8abd8d0e078c92d993ca4c7df9af2c26
pabita_pun <pabita.pun23@gmail.com>
2019-07-28 19:23:03 +0545

    say change

--- Say.php
+++ Say.php
@@ -1,14 +1,11 @@
 <?php
-include 'connection.php'  
-
-if(isset($_POST['submit']))
-{
-  $sub = $_POST['sub'];
-  $feedback = $_POST['feeback'];
-  $email = $_POST['email'];
-  $email = mysqli_real_escape_string($conn,$email);
-  $query = "SELECT * FROM donorprofile WHERE email = '{$email}'";
-    $select_user_query = mysqli_query($conn,$query);
+include 'src/db/connection.php';
+include 'mail.php';
+if (isset($_POST['submit'])) {
+  $header= $_POST['email'];
+  $email = mysqli_real_escape_string($conn,$header);
+  $sql = "SELECT * FROM donorprofile WHERE email = '{$email}'";
+    $select_user_query = mysqli_query($conn,$sql);
     if(!$select_user_query)
     {
         die("QUERY FAILED".mysqli_error($conn));
@@ -17,70 +14,37 @@ if(isset($_POST['submit']))
     $row = mysqli_fetch_array($select_user_query);
     if( $email == $row['email'] )
     {
-     $subject="Feedback";
-     $message="Hey ".$row['username'].","."<br>". $ 
+     $subject=$_POST['subject'];
+     $message=$_POST['givefeedback']; 
       
-    	$mail= new Mail($email,$subject,$message);
+      $mail= new Mail($to_email,$subject,$message,$header); 
       echo "Hey "; 
     }
     else{
-    	echo "Email unavailable";
+      echo "Email unavailable";
     }
-}
-class Mail
-{
-public $to_email;
-public $subject;
-public $message;
-public $headers = $email; /*'From: noreply @ gmail . com';*/
-public function __construct($to_email,$subject,$message)
-{ 
 
-mail($to_email,$subject,$message,$this->headers);
- } 
-
-}
+  $to_email="khullamann5660@gmail.com";
+    
+    $query = "INSERT INTO feedback(feedback_email,subject,feedback_content) VALUES('$header','$subject','$message')";
+    
+    $result = mysqli_query($conn,$query);
+} 
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<title> LogedIn Give feedback</title>
+  <title> LogedIn Give feedback</title>
 </head>
 <body>
-	<form>
-		<input type="text" name="subject" placeholder="Subject" id="sub">
-		<br><br>
-		<textarea rows="5" name="givefeedback" placeholder="Give feedback..." id="feedback"></textarea>
-		<br><br>
-		<button type="submit" name="submit">Send</button>
-	</form>
+  <form>
+    <input type="text" name="subject" placeholder="Subject">
+    <br><br>
+    <textarea rows="5" name="givefeedback" placeholder="Give feedback..."></textarea>
+    <br><br>
+    <button type="submit" name="submit">Send</button>
+  </form>
 </body>
 
 </html>
-
-/*if(isset($_POST['submit']))
-{
-	//$sub = $_POST['sub'];
-    //$email = $_POST['email'];
-    $email = mysqli_real_escape_string($conn,$email);
-    $query = "SELECT * FROM donorprofile WHERE email = '{$email}'";
-    $select_user_query = mysqli_query($conn,$query);
-    if(!$select_user_query)
-    {
-        die("QUERY FAILED".mysqli_error($conn));
-    }
-
-    $row = mysqli_fetch_array($select_user_query);
-    if( $email == $row['email'] )
-    {
-     $subject="Feedback";
-     $message="Hey ".$row['username'].","."<br>". 
-      
-    	$mail= new Mail($email,$subject,$message);
-      echo "Hey "; 
-    }
-    else{
-    	echo "Email unavailable";
-    }
-}*/
