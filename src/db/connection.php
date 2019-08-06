91871a2ba1c7fb8bfaa383913683ecd454ce1d94
gurungena13 <gurungena13@gmail.com>
2019-08-06 08:00:31 +0545

    connection

--- src/db/connection.php
+++ src/db/connection.php
@@ -6,3 +6,9 @@ $dbname = "khullamann";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>
