0e0a7ff5d563d7c5e409d530ec7963a5d006ffa3
pabita_pun <pabita.pun23@gmail.com>
2019-08-12 22:23:56 +0545

    donee reg form

--- src/js/DoneeReg.js
+++ src/js/DoneeReg.js
@@ -1,4 +1,154 @@
-function checkform()
+function validateForm(){
+  var username = document.getElementById('username').value;
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+  var repassword = document.getElementById('repassword').value;
+  var address = document.getElementById('address').value;
+  var contact = document.getElementById('contact').value;
+  var account = document.getElementById('account').value;
+  var errorMessage = document.getElementById('errorMessage');
+
+  if (username=="") {
+    errorMessage.textContent = "username can't be empty.";
+    return false;
+  }
+  if (email=="") {
+    errorMessage.textContent = "Email can't be empty.";
+    return false;
+  }
+    if (password=="") {
+    errorMessage.textContent = "password can't be empty.";
+    return false;
+  }
+    if (repassword=="") {
+    errorMessage.textContent = "password re-type can't be empty.";
+    return false;
+  }
+  if (address=="") {
+    errorMessage.textContent = "address can't be empty.";
+    return false;
+  }
+  if (contact=="") {
+    errorMessage.textContent = "contact can't be empty.";
+    return false;
+  }
+  if (account="") {
+    errorMessage.textContent = "account can't be empty.";
+    return false;
+  }
+    if (password.length<=8) {
+    errorMessage.textContent = "Password length is too short";
+    return false;
+  }
+    if (password.length>=20) {
+    errorMessage.textContent = "Password length is too long";
+    return false;
+  }
+  if (password!= repassword) {
+    errorMessage.textContent = "The two passwords don't match.";
+    return false;
+  }
+    
+        var abc = /^[A-Z][a-z]+\s[A-Z][a-z]+$/;
+    if(!abc.test(fullName)){
+        errorMessage.textContent="Enter valid Full name."
+        return false;
+        
+    }
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(!re.test(email)){
+       errorMessage.textContent="Enter valid Email."
+       return false;  
+    }
+    else{
+        
+        return true;
+    }
+}    
+    
+
+var check = function() {
+  if (document.getElementById('password').value == 
+      document.getElementById('repassword').value) {
+    document.getElementById('message').style.color = 'green';
+    document.getElementById('message').innerHTML = 'matching';
+      return false;
+  } else {
+    document.getElementById('message').style.color = 'red';
+    document.getElementById('message').innerHTML = 'not matching';
+      return false;
+  }
+    return true;
+}
+
+var fieldCheck = function() {
+    if (document.getElementById('username').value == "") {
+        document.getElementById('message1').style.color = 'red';
+        document.getElementById('message1').innerHTML = 'Username cannot be empty';
+    } else {
+        document.getElementById('message1').style.color = 'green';
+        document.getElementById('message1').innerHTML = 'Username is not empty';
+    }
+    
+    if (document.getElementById('email').value == "") {
+        document.getElementById('message2').style.color = 'red';
+        document.getElementById('message2').innerHTML = 'Email cannot be empty';
+    } else {
+        document.getElementById('message2').style.color = 'green';
+        document.getElementById('message2').innerHTML = 'Email is not empty';
+    }
+    
+    if(document.getElementById('password').value == "") {
+        document.getElementById('message3').style.color = 'red';
+        document.getElementById('message3').innerHTML = 'Password cannot be empty';
+
+    } else {
+        document.getElementById('message3').style.color = 'green';
+        document.getElementById('message3').innerHTML = 'Password is not empty';
+    }
+    if (document.getElementById('repassword').value == "") {
+        document.getElementById('message').style.color = 'red';
+        document.getElementById('message').innerHTML = 'Repassword cannot be empty';
+    
+    } else {
+        document.getElementById('message').style.color = 'green';
+        document.getElementById('message').innerHTML = 'Repassword is not empty';
+    }
+    if (document.getElementById('address').value == "") {
+        document.getElementById('message4').style.color = 'red';
+        document.getElementById('message4').innerHTML = 'Address cannot be empty';
+    } else {
+        document.getElementById('message4').style.color = 'green';
+        document.getElementById('message4').innerHTML = 'Address is not empty';
+    }
+    if (document.getElementById('contact').value == "") {
+        document.getElementById('message5').style.color = 'red';
+        document.getElementById('message5').innerHTML = 'Contact cannot be empty';
+    } else {
+        document.getElementById('message5').style.color = 'green';
+        document.getElementById('message5').innerHTML = 'Contact is not empty';
+    }
+    if (document.getElementById('account').value == "") {
+        document.getElementById('message6').style.color = 'red';
+        document.getElementById('message6').innerHTML = 'Account cannot be empty';
+    } else {
+        document.getElementById('message6').style.color = 'green';
+        document.getElementById('message6').innerHTML = 'Account is not empty';
+    }
+    return true;
+}
+
+var loadImage = function(event) {
+      var image = document.getElementById('profilepic');
+      image.src = URL.createObjectURL(event.target.files[0]);
+    };
+
+     var loadFile = function(event) {
+      var image = document.getElementById('document');
+      image.src = URL.createObjectURL(event.target.files[0]);
+    };
+
+/*function checkform()
 {
     var username = document.getElementById('username').value;
     var email = document.getElementById('email').value;
@@ -75,4 +225,4 @@ var check = function() {
 var loadFile = function(event) {
     var image = document.getElementById('pic');
     image.src = URL.createObjectURL(event.target.files[0]);
-};
+};*/
