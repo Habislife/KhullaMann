a722533955b41ff12d8db79be1d05c6dfdad8c5e
pabita_pun <pabita.pun23@gmail.com>
2019-08-14 19:45:31 +0545

    sql push

---
+++ src/db/new.php
@@ -0,0 +1,46 @@
+<?php 
+include 'connection.php';
+include 'mail.php';
+// if(isset($_POST['submit']))
+// {
+//  $email = $_POST['email'];
+//   $email = mysqli_real_escape_string($conn,$email);
+//   $query = "SELECT * FROM donorprofile WHERE email = '{$email}'";
+//     $select_user_query = mysqli_query($conn,$query);
+//     if(!$select_user_query)
+//     {
+//         die("QUERY FAILED".mysqli_error($conn));
+//     }
+
+//     $row = mysqli_fetch_array($select_user_query);
+//     if( $email == $row['email'] )
+//     {
+$email="devkotahancy@gmail.com";
+$subject="Forget Password";
+$from = 'noreply@gmail.com';
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+// Create email headers
+$headers .= 'From: '.$from."\r\n".
+    'Reply-To: '.$from."\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+$message = "<html><body bgcolor='#48B5B1'>";
+$message.="<p>Hi".",</p>";
+$message.="<p>Reset your password</p>";
+$message.="<p>To change your KhullaMann password, click the link below.</p>";
+$message.="<p><a href='localhost/KhullaMann'>Reset my password</a></p>";
+$message.="<p>This link will expire in 24 hours, so be sure to use it right away.</p>";
+$message.="<p> Thank you for using KhullaMann!</p>";
+$message.="<p> The KhullaMann Team</p>";
+$message .= '</body></html>';
+$mail= new Mail($email,$subject,$message,$headers);
+//       echo "Hi "; 
+//     }
+//     else{
+//     	echo "Email unavailable";
+//     }
+// }
+ ?>
+
+
+
