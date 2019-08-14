0394ba749c5e0ae3fc6011ac15f33f2e18eb5176
Bishal Devkota <devkotahancy@gmail.com>
2019-08-14 16:32:54 +0545

    bol

--- src/db/RequestPassword.php
+++ src/db/RequestPassword.php
@@ -14,17 +14,26 @@ if(isset($_POST['submit']))
 
     $row = mysqli_fetch_array($select_user_query);
     if( $email == $row['email'] )
-    {
-     $subject="Forget Password";
-     $message="Hi ".$row['username'].","."<br>".
-"Reset your password"."<br>".", and we'll get you on your way"."<br>".
-"To change your KhullaMann password, click the link below."."<br>"."<a href='localhost/KhullaMann'>.
-Reset my password</a>"."<br>".
-"This link will expire in 24 hours, so be sure to use it right away."."<br>"."<br>".
-"Thank you for using KhullaMann!
-"."<br>"."The KhullaMann Team";
-$headers = 'From: noreply @ gmail . com';
-    	$mail= new Mail($email,$subject,$message,$headers);
+     {
+$email="devkotahancy@gmail.com";
+$subject="Forget Password";
+$from = 'noreply@gmail.com';
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+// Create email headers
+$headers .= 'From: '.$from."\r\n".
+    'Reply-To: '.$from."\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+$message = "<html><body bgcolor='#48B5B1'>";
+$message.="<p>Hi".$row['username'].",</p>";
+$message.="<p>Reset your password</p>";
+$message.="<p>To change your KhullaMann password, click the link below.</p>";
+$message.="<p><a href='localhost/KhullaMann'>Reset my password</a></p>";
+$message.="<p>This link will expire in 24 hours, so be sure to use it right away.</p>";
+$message.="<p> Thank you for using KhullaMann!</p>";
+$message.="<p> The KhullaMann Team</p>";
+$message .= '</body></html>';
+$mail= new Mail($email,$subject,$message,$headers);
       echo "Hi "; 
     }
     else{
