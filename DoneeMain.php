8df04599dc240c6500dba343065ca542eabca761
gurungena13 <gurungena13@gmail.com>
2019-08-29 07:45:59 +0545

    Card added

--- DoneeMain.php
+++ DoneeMain.php
@@ -10,22 +10,43 @@
 <html>
 <head>
   <title>Donee Main Page</title>
-  <link rel="stylesheet" type="text/css" href="src/css/doneeMain.css">
+  <link rel="stylesheet" type="text/css" href="src/css/DoneeMain.css">
   
 </head>
 <body>
+
   <?php include 'Nav.html' ?>
+
   <div class="container">
-    <h3 align="left" style="padding-top: 80px; font-size: 20px; font-family: sans-serif;"><u>Previous Projects</u></h3>
+    <h3 align="center" style="padding-top: 80px; font-size: 20px; font-family: sans-serif;"><u>Previous Projects</u></h3>
+
   <div class="content" align="center" style="margin-top: 30px; border: 2px solid black;">
-    <div class="article">
-      <h2>ARTICLE 1</h2><br><img src="src/images/img2.jpg"><span>vbebjknrknerv</span>
+    <div class="card">
+       <a href="#"><img src="src/images/anes-sabitovic-AyflGfRL9OA-unsplash.jpg" alt="Avatar" style="width:100%"></a>
+        <h1>Refugee Campaign</h1>
+        <p class="bio">Help the refugees.</p>
+         <p>Some text about the jeans. Super slim and comfy lorem ipsum lorem jeansum. Lorem jeamsun denim lorem  Super slim and cfy um v.uper slim and cfy um vuper slim and cfy um upanv</p>
+        <a href="post.php"><button id="viewbtn">View</button></a>
+        <a href="LoginForm.php"><button id="donatebtn">Donate</button></a>
     </div>
-    <div class="article">
-      <h2>ARTICLE 2</h2><br><img src="src/images/img2.jpg"><span>vvrbevgvbtbtrbtrb</span>
+      
+
+      <div class="card">
+       <a href="#"><img src="src/images/anes-sabitovic-AyflGfRL9OA-unsplash.jpg" alt="Avatar" style="width:100%"></a>
+        <h1>Refugee Campaign</h1>
+        <p class="bio">Help the refugees.</p>
+         <p>Some text about the jeans. Super slim and comfy lorem ipsum lorem jeansum. Lorem jeamsun denim lorem  Super slim and cfy um v.uper slim and cfy um vuper slim and cfy um upanv</p>
+        <a href="post.php"><button id="viewbtn">View</button></a>
+        <a href="LoginForm.php"><button id="donatebtn">Donate</button></a>
     </div>
-    <div class="article">
-      <h2>ARTICLE 3</h2><br><img src="src/images/img2.jpg"><span>bfdvehverhbhrttrr</span>
+    
+      <div class="card">
+       <a href="#"><img src="src/images/anes-sabitovic-AyflGfRL9OA-unsplash.jpg" alt="Avatar" style="width:100%"></a>
+        <h1>Refugee Campaign</h1>
+        <p class="bio">Help the refugees.</p>
+         <p>Some text about the jeans. Super slim and comfy lorem ipsum lorem jeansum. Lorem jeamsun denim lorem  Super slim and cfy um v.uper slim and cfy um vuper slim and cfy um upanv</p>
+        <a href="post.php"><button id="viewbtn">View</button></a>
+        <a href="LoginForm.php"><button id="donatebtn">Donate</button></a>
     </div>
   </div>
 
@@ -34,7 +55,7 @@
     <button type="submit" name="submit" class="create"><a href="./doneePage.php"> Create New Post</a></button>
   </div>
 
-  <div class="footer"><h4>Footer</h4></div>
+  
   </div>
   <script type="text/javascript" src="../js/index.js"></script>
 </body>
--- DonorPage.php
+++ DonorPage.php
@@ -9,22 +9,41 @@
 <html>
 <head>
   <title>Donor Page</title>
-  <link rel="stylesheet" type="text/css" href="src/css/donorPage.css">
+  <link rel="stylesheet" type="text/css" href="src/css/DonorPage.css">
 </head>
 <body>
   <?php include 'Nav.html' ?>
 
   <div class="content" align="center" style="margin-top: 80px;">
     <h1>Previous donations</h1>
-    <div class="article">
-      <h1>ARTICLE 1</h1><br><img src="src/images/img2.jpg"><span>vbebjknrknerv</span>
+     <div class="card">
+       <a href="#"><img src="src/images/anes-sabitovic-AyflGfRL9OA-unsplash.jpg" alt="Avatar" style="width:100%"></a>
+        <h1>Refugee Campaign</h1>
+        <p class="bio">Help the refugees.</p>
+         <p>Some text about the jeans. Super slim and comfy lorem ipsum lorem jeansum. Lorem jeamsun denim lorem  Super slim and cfy um v.uper slim and cfy um vuper slim and cfy um upanv</p>
+        <a href="post.php"><button id="viewbtn">View</button></a>
+        <a href="LoginForm.php"><button id="donatebtn">Donate</button></a>
     </div>
-    <div class="article">
-      <h1>ARTICLE 2</h1><br><img src="src/images/img2.jpg"><span>vvrbevgvbtbtrbtrb</span>
+      
+
+      <div class="card">
+       <a href="#"><img src="src/images/anes-sabitovic-AyflGfRL9OA-unsplash.jpg" alt="Avatar" style="width:100%"></a>
+        <h1>Refugee Campaign</h1>
+        <p class="bio">Help the refugees.</p>
+         <p>Some text about the jeans. Super slim and comfy lorem ipsum lorem jeansum. Lorem jeamsun denim lorem  Super slim and cfy um v.uper slim and cfy um vuper slim and cfy um upanv</p>
+        <a href="post.php"><button id="viewbtn">View</button></a>
+        <a href="LoginForm.php"><button id="donatebtn">Donate</button></a>
     </div>
-    <div class="article">
-      <h1>ARTICLE 3</h1><br><img src="src/images/img2.jpg"><span>bfdvehverhbhrttrr</span>
+    
+      <div class="card">
+       <a href="#"><img src="src/images/anes-sabitovic-AyflGfRL9OA-unsplash.jpg" alt="Avatar" style="width:100%"></a>
+        <h1>Refugee Campaign</h1>
+        <p class="bio">Help the refugees.</p>
+         <p>Some text about the jeans. Super slim and comfy lorem ipsum lorem jeansum. Lorem jeamsun denim lorem  Super slim and cfy um v.uper slim and cfy um vuper slim and cfy um upanv</p>
+        <a href="post.php"><button id="viewbtn">View</button></a>
+        <a href="LoginForm.php"><button id="donatebtn">Donate</button></a>
     </div>
+
   </div>
 
   <script type="text/javascript" src="js/index.js"></script>
