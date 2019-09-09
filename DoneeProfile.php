3c7c1ca522481ef79b4b9625fb8c604d4da0f14b
pabita_pun <pabita.pun23@gmail.com>
2019-09-09 15:57:41 +0545

    css n js

--- DoneeProfile.php
+++ DoneeProfile.php
@@ -9,7 +9,7 @@
 <html>
 <head>
 	<title>Donee Profile</title>
-	<link rel="stylesheet" type="text/css" href="src/css/RegisterForm.css">
+	<link rel="stylesheet" type="text/css" href="src/css/doneeProfile.css">
   <script type="text/javascript" src="src/js/doneeProfile.js"></script>
   
 
@@ -18,43 +18,57 @@
    <?php include 'Nav.html' ?>
 
     <div class="box">
-			<form method="POST" action="" >
+			<form method="POST" onsubmit="return validateProfile()" action="" >
 			
 				<h1 align="center">Profile</h1>
 			
+      <div class="inputBox">
 				<div class="image_container">
 			        <div id="upload">
 			            <img id="image">
 			        </div>
 			        <input type="file" name="image" accept="image/*" id="file" onchange="loadFile(event)">
        			 </div>
+       </div>
 
-			 <input type="text" placeholder="Enter Username" name="username" id="username">		
-				<br><span id='message1'></span><br>
+       <div class="inputBox">
+			    <input type="text" placeholder="Enter Username" name="username" id="username" onkeyup='return validateProfile()'>	
+       </div>
 
-			
-   				 <input type="email" placeholder="Enter Email" name="email" id="email" onkeyup='fieldCheck();'>
-   				 <br><span id='message2'></span><br>
+				<span id='message1'></span><br>
 
-    		
-   				 <input type="password" placeholder="Enter Password" name="password" id="password" onkeyup='fieldCheck();'>
-   				 <br><span id='message3'></span><br>
+			  <div class="inputBox">
+   				<input type="email" placeholder="Enter Email" name="email" id="email" onkeyup='return validateProfile()'>
+        </div>
+   				<span id='message2'></span><br>
+
+    		<div class="inputBox">
+   				<input type="password" placeholder="Enter Password" name="password" id="password" onkeyup='return validateProfile()'>
+        </div>
+   				<span id='message3'></span><br>
     			
-   				 <input type="password" placeholder="Enter Re-Password" name="repassword" id="repassword" onkeyup='check();' onkeyup='fieldCheck();'>
-   				 <br><span id='message'></span><br>
+        <div class="inputBox">
+   				 <input type="password" placeholder="Enter Re-Password" name="repassword" id="repassword" onkeyup='check();' onkeyup='return validateProfile()'>
+        </div>
+   				<span id='message'></span><br>
 
-   				
-   				 <input type="text" placeholder="Enter Address" name="address" id="address" onkeyup='fieldCheck();'>
-   				 <br><span id='message4'></span><br>
+   			<div class="inputBox">
+   				 <input type="text" placeholder="Enter Address" name="address" id="address" onkeyup='return validateProfile()'>
+        </div>
+   				<span id='message4'></span><br>
    				 
-   				 <input type="tel" placeholder="Enter Contact No." name="contact" id="contact" onkeyup='fieldCheck();'>
-   				  <br><span id='message5'></span><br>
+        <div class="inputBox">
+   				 <input type="tel" placeholder="Enter Contact No." name="contact" id="contact" onkeyup='return validateProfile()'>
+        </div>
+   				<span id='message5'></span><br>
 
-            <input type="tel" placeholder="Enter Account No." name="account" id="account" onkeyup='fieldCheck();'>
-            <br><span id='message6'></span><br>
+        <div class="inputBox">
+            <input type="tel" placeholder="Enter Account No." name="account" id="account" onkeyup='return validateProfile()'>
+        </div>
+          <span id='message6'></span><br>
 
    				 <div align="center">
-   					 <button type="submit" name="submit" class="register">Save Changes</button>
+   					 <input type="submit" name="submit" class="register" value="save changes">
    					</div>
             
 			</form>
