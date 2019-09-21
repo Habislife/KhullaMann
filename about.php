65566d3fd932cf0f93eb09a12c8380820416ff13
gurungena13 <gurungena13@gmail.com>
2019-09-21 18:45:03 +0545

    About page added

---
+++ about.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Khulla Mann | About </title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+	<link rel="stylesheet" type="text/css" href="src/css/index.css">
+</head>
+<body>
+	<header>
+		<div class="container">
+			<div id="branding">
+				<h1><span class="highlight">Khulla</span>  Mann</h1>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li  class="current"><a href="about.php">About</a></li>
+					<li><a href="Feedback.php">Contact Us</a></li>
+					<li><a href="RegisterForm.php">Become a donor</a></li>
+					<li><a href="DoneeRegForm.php">Sign Up</a></li>
+					<li><a href="LoginForm.php">Login</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+
+	<section id="banner">
+		<div class="container">
+			<h1>Search to Donate</h1>
+			<form>
+				<input type="text" placeholder="Search...">
+				<button type="submit" class="button_1">Search</button>
+			</form>
+		</div>
+	</section>
+
+	<section id="main">
+		<div class="container">
+			<article id="main-col">
+				<h1 class="page-title">About Us</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in </p>
+				<p>
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</article>
+
+			<aside id="sidebar">
+				<div class="dark">
+					<h3>What We Do</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+				</div>
+			</aside>
+		</div>
+	</section>
+
+	<footer>
+		<p>Khulla Mann, Copyright &copy; 2019</p>
+	</footer>
+</body>
+</html>
