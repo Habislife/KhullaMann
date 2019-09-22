e52e36b8bd48b83cb9d2ecb47bfa88893bfcf759
pabita_pun <pabita.pun23@gmail.com>
2019-09-23 00:07:30 +0545

    done

--- admin/profile.php
+++ admin/profile.php
@@ -2,7 +2,34 @@
 <html>
 <head>
 	<title>Profile</title>
-	<link rel="stylesheet" type="text/css" href="src/Profile.css">
+	<!-- <link rel="stylesheet" type="text/css" href="src/css/Profile.css">
+     -->
+     <style type="text/css">
+        .container {
+          width: 60%;
+          height: 400px;
+          border: 2px solid black;
+          font-size: 40px;
+          font-family: sans-serif;
+          padding-left: 20px;
+          padding-top: 50px;
+          margin: 100px 5px 5px 250px; 
+          background-color: lightcyan;
+        }
+        .container a{
+            text-decoration: none;
+            margin: 600px 10px 5px 40px;
+            color: green;
+        }
+
+        .content{
+          width: 350px;
+          height: 250px;
+          border: 1px solid black;
+          margin: 10px 20px 30px 30px;
+          float: right;
+        }
+     </style>
 </head>
 <body>
 	<?php 
@@ -14,15 +41,16 @@
     $row = $result->fetch_assoc();
     ?>
 
-    <div class="container">
+    <div class="container" align="center">
     	<div class="content">
-    		<?php echo "<img src='".$row["document"]."' height='500' width='500'>";
+    		<?php echo "<img src='".$row["document"]."' height='250' width='350'>";
             echo"<br>"; ?>
     	</div>
     	<?php 
+        echo "<br><br>";
     	echo $row['username'];
-    	echo "<a href='transfer.php?value_key=".$var."'>"."Approve</a>";
-        echo"<br>";
+        echo"<br><br><br><br><br>";
+    	echo "<a href='transfer.php?value_key=".$var."'>"."Approve</a>";  
         echo "<a href='delete.php?value_key=".$var."'>"."Delete</a>"; 
         ?>    
     </div>
